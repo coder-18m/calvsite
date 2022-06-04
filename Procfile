940c3e13e7aa7f6a4354f92de2f576ee2db88160
Claire Miller <clairemiller@outlook.com>
2022-06-03 22:22:00 -0700

    tweaked app for heroku

---
+++ Procfile
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cd7bef71600d9fee02e080ce769f6a8aabb50b6efee53f88477dd969d7dbacb1
+size 26
--- mysite/settings.py
+++ mysite/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17d50c2566bb88b52fe76c1d0b9034da2d0b549888890d9d467bc2d98f79c3b2
-size 3469
+oid sha256:c613e9501aa6d2fb0f74794a47d3484325b52889b06ce12bca726226affe3f07
+size 3709
