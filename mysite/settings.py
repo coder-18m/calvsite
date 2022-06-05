dc134e25ac9bc0883752a72fb1235f5444bc9801
Claire Miller <clairemiller@outlook.com>
2022-06-05 05:03:50 -0700

    Major upadates to login page, fixed image post bugs

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c613e9501aa6d2fb0f74794a47d3484325b52889b06ce12bca726226affe3f07
-size 3709
+oid sha256:b5da61b8566fbe342457fda4db20b0c9bfb9aac399581f7c20f7c93e0f7270ee
+size 3680
--- thesite/views.py
+++ thesite/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6074fead4a8861a376b290f83fcd565d954f5c6f9f8b187df70ddba57bcec5f1
-size 2090
+oid sha256:aa9a57904edfe0cba34878c093dd6eb89903f7dc607ec015efa5ae42e5fcdc2c
+size 2318
