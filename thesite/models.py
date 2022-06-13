14105ff7ba0b7980de6a9a4d7e8308b93a416575
Claire Miller <clairemiller@outlook.com>
2022-06-12 23:50:32 -0700

    added profile info on article details

--- thesite/models.py
+++ thesite/models.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb6ebf937f2d85c4dacb35d249e0e5e00c9df81a26f45addfbe557ccdb813a05
-size 1192
+oid sha256:15b1d7a1c085dd3d2667a3a424d8ffb6abb9183abd4f4dffd2c182e544448b05
+size 1771
