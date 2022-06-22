42e700902e16852eabe360e37830230ddca8832f
Claire Miller <clairemiller@outlook.com>
2022-06-21 23:33:04 -0700

    added programming to contact form

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87652379643570776775956f467b8bc184ccbf1890562b6dbf68532f407ecf2c
-size 3708
+oid sha256:dc93d8c5449acad43c9e0ae838ab6fa6c8c3ebe942406f7b68ed4a74ab80391e
+size 3892
--- thesite/views.py
+++ thesite/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25880291588e93cc660660277d3e4a8aaa81df8ae5db7acd7fd8a28d603c648b
-size 4681
+oid sha256:87459fb05e2b5a12e2906a1328b06b3cb9dbfaa6d5770ba427d5308d53db5906
+size 5248
