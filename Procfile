67610522d70ecd771075635f309e1f23d2a60f7a
Claire Miller <clairemiller@outlook.com>
2022-07-08 00:30:59 -0700

    fc

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdd37e9fb09812d3bda1381db2267c8cbc86a441bfdd993a4c250835d4365714
-size 50
+oid sha256:cd7bef71600d9fee02e080ce769f6a8aabb50b6efee53f88477dd969d7dbacb1
+size 26
--- mysite/settings.py
+++ mysite/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4048f3c357ee2a609b9a88dfb78409e1025811f6a966df6cde22bf0b235a338
-size 4391
+oid sha256:3740250faabd47eb9c446ec7a442c25d226778476a52d4c167981883a35650fd
+size 4480
