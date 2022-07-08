46c393f57235fa88440b89ddc74fb7d56229ca18
Claire Miller <clairemiller@outlook.com>
2022-07-08 00:42:51 -0700

    heroku push again

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15e02d38d3341d0de757d613d5a625d9b1a272b6c9cf34b36a7c05b35845f8b5
-size 4529
+oid sha256:a6258c97fae4f6181d74212f5fac2383789c4404cd258d34ad886ee77aaa0efe
+size 4486
